fc3a22eb470e13d85c1b5161c908874c3ecbaae4
Espen Strømland <espen.stromland@gmail.com>
2022-10-09 21:01:56 +0200

    feat: random-letter-picker

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Button, Carousel } from "react-bootstrap";
+import "./App.css";
+import { letters } from "./letters";
+
+function randomLetter(
+  set: (letter: number) => void,
+  setLast: (last: boolean) => void
+) {
+  const numberOfLetters = 5;
+  const timeOfShow = 4000;
+  const cache: Set<number> = new Set();
+
+  setLast(false);
+  const index = Math.floor(Math.random() * 29);
+  cache.add(index);
+  set(index);
+
+  const inter = setInterval(() => {
+    let chosen: number | undefined;
+    do {
+      chosen = Math.floor(Math.random() * 29);
+      cache.add(chosen);
+    } while (!cache.has(chosen));
+    set(chosen);
+  }, timeOfShow);
+
+  setTimeout(() => {
+    clearInterval(inter);
+    setLast(true);
+  }, timeOfShow * numberOfLetters + timeOfShow / 2);
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Letters />
     </div>
+  );
+}
+
+function Letters() {
+  const [letter, setLetter] = useState<number>();
+  const [last, setLast] = useState(false);
+  return (
+    <Carousel
+      interval={null}
+      activeIndex={letter ?? 29}
+      indicators={false}
+      controls={false}
+    >
+      {letters.map((it) => (
+        <Carousel.Item key={it}>
+          <div
+            style={{
+              height: "100vh",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              background: last ? "#697F3F" : "#C4502A",
+            }}
+          >
+            <h1 style={{ fontSize: "200px" }}>{it}</h1>
+          </div>
+        </Carousel.Item>
+      ))}
+      <Carousel.Item>
+        <div
+          style={{
+            height: "100vh",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            background: "#F0E6C8",
+          }}
+        >
+          <Button
+            style={{ fontSize: "80px" }}
+            onClick={() => randomLetter(setLetter, setLast)}
+          >
+            Start
+          </Button>
+        </div>
+      </Carousel.Item>
+    </Carousel>
   );
 }
 
